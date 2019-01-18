82d9cf8d4f3bc68fe2c9a2f503d303c00947a9f1
vagrant <tom.w.j.sanderson@gmail.com>
2019-01-18 22:47:04 +0000

    updated db with review table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190117162146) do
+ActiveRecord::Schema.define(version: 20190118224425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 20190117162146) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.text     "description"
+    t.integer  "rating"
+    t.datetime "createdat"
+    t.datetime "updated"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
